83cae0bfb40717ada6d773e5f208ff4352e0fca1
Eric Serge Uwimana <euwimana@cardinalpeak.com>
2023-02-21 21:22:36 -0700

    Added Lambda Function

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-
+import { LambdaStack } from './stacks/lambda-stack';
 import { WebsiteBucketStack } from './stacks/website-bucket-stack'
  
 const app = new cdk.App();
 
-new WebsiteBucketStack(app, 'WebsiteBucketStack')
+new WebsiteBucketStack(app, 'WebsiteBucketStack');
 
-app.synth()
+new LambdaStack(app, 'LambdaStack');
+
+app.synth();
