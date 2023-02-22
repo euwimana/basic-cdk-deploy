efe67c005dc1ab50ec3e73c3f5e802698f62ee30
Eric Serge Uwimana <euwimana@cardinalpeak.com>
2023-02-21 22:14:37 -0700

    Added API Gateway

--- assets/lambda/index.js
+++ assets/lambda/index.js
@@ -3,5 +3,15 @@ const { quotes } = require('./quotes');
 exports.handler = async (event, context) => {
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
-  return { statusCode: 200, body: quote }
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,GET',
+    },
+    isBase64Encoded: false,
+    body: JSON.stringify(quote),
+  }
 }
--- src/stacks/lambda-stack.ts
+++ src/stacks/lambda-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { Cors, LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
@@ -7,10 +8,23 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new Function(this, 'Lambda', {
+    const lambda = new Function(this, 'Lambda', {
       runtime: Runtime.NODEJS_16_X,
       code: Code.fromAsset('./assets/lambda'),
       handler: 'index.handler',
     });
+
+    const api = new LambdaRestApi(this, 'RestApi', {
+      handler: lambda,
+      proxy: false,
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+    });
+
+    const quotes = api.root.addResource('quotes');
+
+    quotes.addMethod('GET')
   }
 }
