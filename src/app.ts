63602b6e433cbbfcab13ca28bf5fc5fd6d57c8ee
Eric Serge Uwimana <euwimana@cardinalpeak.com>
2023-02-21 17:16:05 -0700

    Added Website Bucket

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+
+import { WebsiteBucketStack } from './stacks/website-bucket-stack'
  
 const app = new cdk.App();
+
+new WebsiteBucketStack(app, 'WebsiteBucketStack')
+
+app.synth()
